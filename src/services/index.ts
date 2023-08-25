dd508908a23d40ee96d1ffd2a48867e20224cbf6
Aaditya Buchale <aadityabuchale15@gmail.com>
2023-08-25 11:11:46 +0530

    deploying code

--- src/services/index.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const COUNTRY_API_ENDPOINT = "https://restcountries.com/v3.1/";
-
-export function getAllCountries() {
-    // will call api and get results
-    return axios.get(COUNTRY_API_ENDPOINT + "all");
-}
-
